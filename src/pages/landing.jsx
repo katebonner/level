bed1b63d5e1bfef553ccce8f49a68a44110165a9
Kate Bonner <katebonner277@gmail.com>
2022-10-12 16:20:23 -0400

    added modal

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -3,8 +3,23 @@ import SpotifyAuth from '../spotify/auth';
 
 const Landing = () => {
     const spotifyAuth = SpotifyAuth();
+
+    const exitModal = () => {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+    }
+
+
     return(
-        <>
+        <>  
+            <div id="myModal" className="modal">
+                 <div className="modal-content">
+                    <span onClick={exitModal} className="close">&times;</span>
+                    <p className='modal-text'>Due to restrictions with the Spotify API, only a limmited list of specified users can access this applicaiton. To request access, please email me at <a className='modal-link' href='mailto:katebonner277@gmail.com'>katebonner277@gmail.com</a></p>
+                    </div>
+            </div>
+
+
             <h1 className='logo floating'>level</h1>
             <img className='wave floating2' src={require('../assets/wave.png')} alt='sin wave'/>
             <div className='center'>
