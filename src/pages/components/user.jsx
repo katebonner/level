262e11bb346701fda72ec80f5e8c032eb16ac8a0
Kate Bonner <katebonner277@gmail.com>
2022-10-13 12:41:44 -0400

    edge case of no profile photo

--- src/pages/components/user.jsx
+++ src/pages/components/user.jsx
@@ -10,17 +10,29 @@ const User = () => {
   
     return (
       <>
-        {isLoading ?  <h1>loading...</h1> :
-          <>
+        {isLoading ? 
+          <div key={data?.id} className='me-container'>
+            <div>
+              <h1 className='floating no-padding'>loading profile ...</h1>
+              <img className='wave no-padding floating2' src={require('../../assets/wave.png')} alt='sin wave'/>
+            </div>
+            <Link to='/score'><button disable='true' className='valence-btn no-padding'>valence</button></Link>
+            <div className='orbit circle'></div>
+          </div>
+        
+        :
           <div key={data?.id} className='me-container'>
             <div>
               <h1 className='floating no-padding'>{data?.display_name}</h1>
               <img className='wave no-padding floating2' src={require('../../assets/wave.png')} alt='sin wave'/>
-              </div>
-              <Link to='/score'><button className='valence-btn no-padding'>calculate valence</button></Link>
-              <img className='orbit' src={data?.images[0].url} alt='profile'/> 
+            </div>
+            <Link to='/score'><button className='valence-btn no-padding'>calculate valence</button></Link>
+            {data.images[0].url ? <img className='orbit' src={data.images[0].url} alt='profile'/> 
+            :
+            <div className='orbit circle'></div>
+            }
           </div> 
-          </>}
+        }
       </>
     )
 }
--- src/spotify/auth.js
+++ src/spotify/auth.js
@@ -1,8 +1,8 @@
 const Auth = () => {
     return {
         CLIENT_ID:'545605ad511f4995ac8c5097099e56e6',
-        // REDIRECT_URI: 'http://localhost:3000',
-        REDIRECT_URI: 'https://boiling-retreat-31003.herokuapp.com',
+        REDIRECT_URI: 'http://localhost:3000',
+        // REDIRECT_URI: 'https://boiling-retreat-31003.herokuapp.com',
         AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
         RESPONSE_TYPE: 'token',
         SCOPE:'user-top-read user-read-email'
