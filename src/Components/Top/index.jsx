cdc0f3083c61055d2d90ed5ee9c73e5fed5267c1
kate bonner <katebonner277@gmail.com>
2023-09-01 01:25:24 -0400

    3D album covers

--- src/Components/Top/index.jsx
+++ src/Components/Top/index.jsx
@@ -1,22 +1,71 @@
-import React from "react";
+import React, { useRef, useMemo } from "react";
 import { useQuery } from "react-query";
 import queryTop from "../../api/queryTop";
-import { AlbumCovers, RowWrap } from "./elements";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { TextureLoader, DoubleSide, Vector3 } from "three";
+import { Circle, Circle2, Circle3 } from "../../Controllers/Landing/elements";
+
+const AlbumParticle = ({ url }) => {
+  const texture = useMemo(() => {
+    if (url) {
+      return new TextureLoader().load(url);
+    }
+    return null;
+  }, [url]);
+
+  const mesh = useRef();
+  const initialPos = new Vector3(
+    (Math.random() - 0.5) * 10,
+    (Math.random() - 0.5) * 10,
+    (Math.random() - 0.5) * 10
+  );
+  const velocity = new Vector3(
+    (Math.random() - 0.5) * 0.05,
+    (Math.random() - 0.5) * 0.05,
+    (Math.random() - 0.5) * 0.05
+  );
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.position.add(velocity);
+      if (mesh.current.position.length() > 5) {
+        velocity.negate();
+      }
+    }
+  });
+
+  if (!texture) return null;
+
+  return (
+    <mesh ref={mesh} position={initialPos}>
+      <boxGeometry args={[0.05, 1]} />
+      <meshStandardMaterial side={DoubleSide} map={texture} />
+    </mesh>
+  );
+};
 
 const Top = () => {
   const { data, isLoading } = useQuery("top", async () => await queryTop());
 
+  const albumCovers = data?.items.map((item) => item.album.images[0].url);
+
   return (
-    <RowWrap>
-      {!isLoading &&
-        data?.items.map((item) => (
-          <AlbumCovers
-            src={item.album.images[0].url}
-            alt="album cover"
-            key={item.id}
-          />
-        ))}
-    </RowWrap>
+    <>
+      <Circle />
+      <Circle2 />
+      <Circle3 />
+      {!isLoading && (
+        <Canvas style={{ width: "100vw", height: "100vh" }}>
+          <ambientLight intensity={0.8} />
+          <pointLight position={[3, 0, 5]} color="yellow" intensity={3} />
+          <pointLight position={[-5, 0, -5]} color="blue" intensity={5} />
+          <pointLight position={[0, 5, 0]} color="red" intensity={5} />
+          {albumCovers.map((url, index) => (
+            <AlbumParticle key={index} url={url} />
+          ))}
+        </Canvas>
+      )}
+    </>
   );
 };
 
