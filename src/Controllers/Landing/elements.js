cc8104be047b1b4f4ad26848c9ef06da1b040b1e
kate bonner <katebonner277@gmail.com>
2023-09-01 00:41:45 -0400

    landing page face lift

--- src/Controllers/Landing/elements.js
+++ src/Controllers/Landing/elements.js
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
 
 export const Logo = styled.div({
-  fontSize: "25vw",
+  fontSize: "8vw",
   fontWeight: 600,
-  paddingBottom: 0,
-  marginBottom: 0,
+  position: "absolute",
+  left: "60px",
+  marginTop: "10%",
+  opacity: 0.6,
 });
 
 export const Circle = styled.div({
@@ -14,39 +16,44 @@ export const Circle = styled.div({
   width: "120vh",
   height: "120vh",
   borderRadius: "50%",
-  backgroundColor: "#006641",
+  backgroundColor: "yellow",
+  opacity: "0.5",
+  filter: "blur(200px)",
 });
 
-export const ConnectButton = styled.button({
-  backgroundColor: "transparent",
-  textDecoration: "none",
-  padding: "20px 30px",
-  borderRadius: 200,
-  borderWidth: 1,
-  borderColor: "#fff",
-  borderStyle: "solid",
-  fontSize: "2vw",
-  zIndex: 5,
-  fontWeight: 600,
-  color: "#fff",
-  "&:hover": {
-    backgroundColor: "#de49a2",
-    borderColor: "transparent",
-  },
-  "@media (max-width: 900px)": {
-    fontSize: "4vw",
-  },
+export const Circle2 = styled.div({
+  position: "fixed",
+  bottom: "-10vh",
+  left: "-70vh",
+  width: "120vh",
+  height: "120vh",
+  borderRadius: "50%",
+  backgroundColor: "blue",
+  opacity: "0.4",
+  filter: "blur(200px)",
+});
+
+export const Circle3 = styled.div({
+  position: "fixed",
+  top: "-70vh",
+  left: "0vh",
+  width: "120vh",
+  height: "120vh",
+  borderRadius: "50%",
+  backgroundColor: "red",
+  opacity: "0.5",
+  filter: "blur(200px)",
 });
 
 export const Description = styled.div({
+  fontSize: "2vw",
   fontWeight: 600,
-  fontSize: "2.5vw",
-  width: "70%",
-  textAlign: "center",
-  margin: "0 0 3vh 0",
-  zIndex: 1,
+  position: "absolute",
+  left: "64px",
+  marginTop: "20%",
+  opacity: 0.6,
   "@media (max-width: 900px)": {
-    fontSize: "5vw",
-    width: "60%",
+    fontSize: "3.2vw",
+    marginTop: "22%",
   },
 });
--- src/Controllers/Landing/index.jsx
+++ src/Controllers/Landing/index.jsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import SpotifyAuth from "../../spotify/auth";
-import { Circle, Description, Quote, Logo, ConnectButton } from "./elements";
+import { Circle, Circle2, Circle3, Description, Logo } from "./elements";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 const Landing = () => {
   const spotifyAuth = SpotifyAuth();
 
+  const Cube = () => {
+    const meshRef = useRef();
+    const [isHovered, setHovered] = useState(false);
+
+    // Rotating animation
+    useFrame(() => {
+      if (meshRef.current) {
+        const rotationSpeed = isHovered ? 0.0005 : 0.005;
+        meshRef.current.rotation.x += rotationSpeed;
+        meshRef.current.rotation.y += rotationSpeed;
+      }
+    });
+
+    const handleCubeClick = () => {
+      window.location.href = `${spotifyAuth.AUTH_ENDPOINT}?client_id=${spotifyAuth.CLIENT_ID}&redirect_uri=${spotifyAuth.REDIRECT_URI}&response_type=${spotifyAuth.RESPONSE_TYPE}&scope=${spotifyAuth.SCOPE}`;
+    };
+
+    return (
+      <mesh
+        ref={meshRef}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+        onClick={handleCubeClick}
+        scale={1}
+      >
+        <torusKnotGeometry args={[3, 0.4, 300, 300]} />
+        <meshStandardMaterial color="#ab577e" metalness={0.5} roughness={0} />
+      </mesh>
+    );
+  };
+
   return (
     <>
       <Circle />
-      <Logo className="floating center">level</Logo>
-      <div className="container">
-        <Description>
-          A portrait of your sound: Dissect the audio features of your top 50
-          tracks on Spotify and discover what truly resonates with you.
-        </Description>
-        <ConnectButton>
-          <a
-            className="bold"
-            href={`${spotifyAuth.AUTH_ENDPOINT}?client_id=${spotifyAuth.CLIENT_ID}&redirect_uri=${spotifyAuth.REDIRECT_URI}&response_type=${spotifyAuth.RESPONSE_TYPE}&scope=${spotifyAuth.SCOPE}`}
-          >
-            CONNECT TO SPOTIFY
-          </a>
-        </ConnectButton>
-      </div>
+      <Circle2 />
+      <Circle3 />
+      <Canvas
+        style={{ width: "100vw", height: "100vh" }}
+        shadows
+        camera={{ position: [0, 0, 5] }}
+      >
+        <ambientLight intensity={3} />
+        <pointLight position={[3, 0, 5]} color="yellow" intensity={30} />
+        <pointLight position={[-5, 0, -5]} color="blue" intensity={50} />
+        <pointLight position={[0, 5, 0]} color="red" intensity={50} />
+        <Cube />
+      </Canvas>
+      <Logo>SYNETHESIA</Logo>
+      <Description>tap the spiral to sync with spotify</Description>
     </>
   );
 };
